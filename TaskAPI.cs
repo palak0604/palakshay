728f6f9bf14669329e12734d1fc82345509ee6c0
Palak Gupta <t-palgup@microsoft.com>
2020-05-14 22:18:58 +0530

    Added mail feature

--- TaskAPI.cs
+++ TaskAPI.cs
@@ -7,6 +7,9 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
 
 namespace PalakshayAPI
 {
@@ -27,7 +30,30 @@ namespace PalakshayAPI
                 contactDetails = data.contactDetails,
                 email = data.email
             };
-            
+            string to = "palakgupta0604@gmail.com"; 
+            string from = newUser.email;  
+            MailMessage message = new MailMessage(from, to);
+
+            string mailbody = newUser.description + "\n" + " Contact Details : " + newUser.contactDetails;
+            message.Subject = "Sending Email Using Asp.Net & C#";
+            message.Body = mailbody;
+            message.BodyEncoding = Encoding.UTF8;
+            message.IsBodyHtml = true;
+            SmtpClient client = new SmtpClient("smtp.gmail.com", 587); //Gmail smtp    
+            System.Net.NetworkCredential basicCredential1 = new
+            System.Net.NetworkCredential(to, "Enter gmail account password here.");
+            client.EnableSsl = true;
+            client.UseDefaultCredentials = false;
+            client.Credentials = basicCredential1;
+            try
+            {
+                client.Send(message);
+            }
+
+            catch (Exception ex)
+            {
+                throw ex;
+            }
             return  newUser!= null
                 ? (ActionResult)new OkObjectResult("Hello " + newUser.userName + "\n"+" specified description is : "+ newUser.description + "\n" + " your contact details are: "+ newUser.contactDetails+ "\n" + " your email ID is: " +newUser.email)
                 : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
